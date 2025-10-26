e2e2ce20f08dda021e7709e372031b47d67b08a8
majochem <77203255+majochem@users.noreply.github.com>
2025-10-26 17:39:51 +0100

    Add `hasTag` function to `modLib` in `ModTools.lua`
    There was no easy existing way to check if a specific tag is already
    present on a on mod (Or I couldn't find it)
    So I added a function for it, which makes it easy to check for, add, or
    replace/modify specific tags
    
    Note: This commit was copied from Pob2 repo

--- src/Modules/ModTools.lua
+++ src/Modules/ModTools.lua
@@ -221,3 +221,35 @@ function modLib.setSource(mod, source)
 	end
 	return mod
 end
+
+-- Check if a mod contains a specific tag already
+-- Note: All keys AND values need to be matched exactly
+---@param mod table individual mod that is to be checked
+---@param searchTag table exact tag you're searching for, e.g.  { type = "Condition", var = "Shocked" }
+---@return boolean @returns `true` if tag is found, otherwise `false`
+---@return number @returns position as `number` if tag exists or `0` if not 
+function modLib.hasTag(mod, searchTag)
+	local numSearchKeys = 0 -- Apparently Lua doesn't have a built in functionality to return the number of keys in a table(?) so have to determine manually
+	for _, __ in pairs(searchTag) do
+		numSearchKeys = numSearchKeys + 1
+	end
+
+	for i, tag in ipairs(mod) do
+		local numTagKeys = 0 -- Total number of keys in tag
+		local numMatchedKeys = 0 -- Number of keys matching with searchTag
+		if type(tag) == "table" then
+			for key, value in pairs(searchTag) do
+				numTagKeys = numTagKeys + 1
+				if tag[key] and (tag[key] == value) then
+					numMatchedKeys = numMatchedKeys + 1
+				else
+					break -- this is not the correct tag
+				end
+			end
+		end
+		if (numSearchKeys > 0) and (numSearchKeys == numMatchedKeys) and (numSearchKeys == numTagKeys) then
+			return true, i
+		end
+	end
+	return false, 0
+end
